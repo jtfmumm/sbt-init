a8b18d33c16fd4e3be525fdfcc18059a8aff17b7
John Mumm <jtfmumm@gmail.com>
2015-07-25 17:46:01 -0400

    Update sbt-init.sh

--- sbt-init/sbt-init.sh
+++ sbt-init/sbt-init.sh
@@ -18,7 +18,11 @@ fi
 mkdir $NAME
 cd $NAME
 
-echo "target/ \n project/ \n.gitignore\n *.iml\n .idea/" > .gitignore
+echo "target/ \n 
+      project/ \n
+      .gitignore \n 
+      *.iml \n 
+      .idea/" > .gitignore
 
 mkdir target
 mkdir project
